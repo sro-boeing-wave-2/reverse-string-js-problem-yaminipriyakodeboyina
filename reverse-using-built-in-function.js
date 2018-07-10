057d8cdfea8f8d956a0c3a6872fb3d709fb02247
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-10 14:52:46 +0530

    reverse string first commit

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,10 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (s) => {
+  let str = s.split('');
+  str = str.reverse();
+  str = str.join('');
+  return str;
+};
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,11 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (s) => {
+  let st;
+  st = '';
+  for (let i = s.length - 1; i >= 0; i -= 1) {
+    st += s[i];
+  }
+  return st;
+};
 
 module.exports = reverse;
